42a63b2da373be096f4d07228f9873e521f86825
AlexHorodnic <alexhorodnic@protonmail.com>
2022-06-02 10:55:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+function increaseFontSizeBy1px() {
+    txt = document.getElementById('a');
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize + 1) + 'px';
+}
+function increaseFontSizeBy2px() {
+    txt = document.getElementById('b');
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize + 1) + 'px';
+}
+function increaseFontSizeBy3px() {
+    txt = document.getElementById('c');
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize + 1) + 'px';
+}
+function increaseFontSizeBy4px() {
+    txt = document.getElementById('d');
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize + 1) + 'px';
+}
+function decreaseFontSizeBy1px() {
+    txt = document.getElementById('a');
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize - 1) + 'px';
+}
+function decreaseFontSizeBy2px() {
+    txt = document.getElementById('b');
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize - 1) + 'px';
+}
+function decreaseFontSizeBy3px() {
+    txt = document.getElementById('c');
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize - 1) + 'px';
+}
+function decreaseFontSizeBy4px() {
+    txt = document.getElementById('d');
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize - 1) + 'px';
+}
